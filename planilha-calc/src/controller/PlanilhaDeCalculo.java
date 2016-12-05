dd11a51df96b1f074607a1e0c878cfc7caa776ee
Laura Abitante <laura.abitante@gmail.com>
2016-12-05 17:58:10 -0200

    Creating dynamic columns

--- planilha-calc/src/controller/PlanilhaDeCalculo.java
+++ planilha-calc/src/controller/PlanilhaDeCalculo.java
@@ -5,12 +5,36 @@ import algorithms.InvalidExpression;
 import datastructures.Celula;
 import datastructures.Coluna;
 import datastructures.ListaEncadeada;
+import datastructures.Node;
 import model.SpreadsheetModel;
 import view.Spreadsheet;
 
 public class PlanilhaDeCalculo {
 
 	private ListaEncadeada<Coluna> colunas = new ListaEncadeada<>();
+
+	private String getColumnName(String cell) {
+		String columnName = cell.substring(0, 1);
+		return columnName;
+	}
+	
+	// Search for a column with a given name. E.g: "A", "K", and so on.
+	private Coluna getColumn(String columnName){
+		columnName = getColumnName(columnName);
+		Node<Coluna> node = colunas.primeiro;
+		for(int i=0; i < colunas.totalDeElementos; i++){
+			if(node.valor().getColuna().equals(columnName)){
+				return node.valor();
+			}
+			else{
+				node = node.proximo();
+			}
+		}
+		// In case of no columns with this name, create a new column and add it to the list.
+		Coluna novaColuna = new Coluna(getColumnName(columnName));
+		colunas.adiciona(novaColuna);
+		return novaColuna;
+	}
 
 	public static void main(String[] args) {
 		(new PlanilhaDeCalculo()).run();
@@ -31,12 +55,17 @@ public class PlanilhaDeCalculo {
 
 			@Override
 			public void setFormula(String cell, String formula) {
-				System.out.println("Celula: " + cell + " - Formula: " + formula);
-				try {
-					System.out.println("Valor: " + ExpressionEvaluator.compute(formula));
-				} catch (InvalidExpression e) {
-					System.out.println("Erro na expressão!");
-				}
+				
+				System.out.println(getColumn(cell));
+				System.out.println(colunas.toString());
+				
+				
+//				System.out.println("Celula: " + cell + " - Formula: " + formula);
+//				try {
+//					System.out.println("Valor: " + ExpressionEvaluator.compute(formula));
+//				} catch (InvalidExpression e) {
+//					System.out.println("Erro na expressão!");
+//				}
 			}
 		};		
 		
--- planilha-calc/src/datastructures/Coluna.java
+++ planilha-calc/src/datastructures/Coluna.java
@@ -5,6 +5,10 @@ public class Coluna {
 	private ListaEncadeada<Celula> celulas = new ListaEncadeada<>();
 	private String coluna;
 	
+	public Coluna (String coluna) {
+		this.coluna = coluna;
+	}
+	
 	public String getColuna() {
 		return coluna;
 	}
